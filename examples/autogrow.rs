a09d6142f5bf836904e146f8f1a37b475af8e33f
Darshan Parajuli <parajulidarshan@gmail.com>
2017-09-30 23:00:23 -0700

    added an example for auto-grow functionality

---
+++ examples/autogrow.rs
@@ -0,0 +1,33 @@
+extern crate jobpool;
+
+use jobpool::JobPool;
+use std::thread;
+use std::time::Duration;
+use std::sync::mpsc;
+
+fn main() {
+    let pool_size = 8; // or number of cpu cores
+    let mut pool = JobPool::new(pool_size);
+    pool.auto_grow(100);
+
+    let (tx, rx) = mpsc::channel();
+
+    for i in 0..100 {
+        let tx = tx.clone();
+        pool.queue(move || {
+            thread::sleep(Duration::from_millis(100));
+            println!("sending: {}", i);
+            tx.send(i).unwrap();
+        });
+    }
+
+    for _ in 0..100 {
+        match rx.recv() {
+            Ok(val) => println!("received: {}", val),
+            Err(e) => eprintln!("Error: {}", e),
+        }
+    }
+
+    pool.shutdown();
+    println!("JobPool has shutdown!");
+}
